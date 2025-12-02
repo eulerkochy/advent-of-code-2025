17771417143d7bafd5545af5d22ae465cfac936e
Koustav Chowdhury <koustav@windfall.capital>
2025-12-02 15:52:57 +0000

    Refactor day-2 puzzle-2 to remove locks from input

--- day-2/puzzle-2.cpp
+++ day-2/puzzle-2.cpp
@@ -1,11 +1,6 @@
-#include "boost/unordered/unordered_flat_map_fwd.hpp"
 #include <bits/stdc++.h>
 #include <boost/algorithm/string/classification.hpp>
-#include <boost/algorithm/string/join.hpp>
 #include <boost/algorithm/string/split.hpp>
-#include <boost/unordered/unordered_flat_map.hpp>
-
-#include <mutex>
 #include <ranges>
 #include <string_view>
 #include <thread>
@@ -34,75 +29,50 @@ auto read_input() -> vector<tuple<long long, long long>> {
   return ranges;
 }
 
-auto number_of_digits(auto number) -> long long {
-  return std::to_string(number).length();
+static constexpr auto kMaxDigits = 31;
+static std::array<std::vector<long long>, kMaxDigits> divisors_table = {};
+
+auto get_divisors(long long n) -> const std::vector<long long> & {
+  return divisors_table[n];
 }
 
-boost::unordered_flat_map<long long, vector<long long>> divisors_map;
-std::mutex divisors_map_mutex;
-
-auto get_divisors(auto number) -> vector<long long> {
-  {
-    std::lock_guard<std::mutex> lock(divisors_map_mutex);
-    if (divisors_map.contains(number)) {
-      return divisors_map[number];
-    }
-  }
-  auto divisors = vector<long long>();
-  for (auto i = 1ll; i * i <= number; i++) {
-    if (number % i == 0) {
-      divisors.push_back(i);
-      if (i != number / i) {
-        divisors.push_back(number / i);
+auto precompute_divisors() -> void {
+  auto compute_divisors = [](auto number) -> std::vector<long long> {
+    auto divisors = std::vector<long long>();
+    for (auto i = 1ll; i * i <= number; i++) {
+      if (number % i == 0) {
+        divisors.push_back(i);
+        if (i != number / i) {
+          divisors.push_back(number / i);
+        }
       }
     }
+    std::ranges::sort(divisors);
+    return divisors;
+  };
+  for (auto i = 1ll; i <= kMaxDigits; i++) {
+    divisors_table[i] = compute_divisors(i);
   }
-  std::ranges::sort(divisors);
-  {
-    std::lock_guard<std::mutex> lock(divisors_map_mutex);
-    divisors_map[number] = divisors;
-  }
-  return divisors;
 }
 
-boost::unordered_flat_map<std::string,
-                          boost::unordered_flat_map<long long, bool>>
-    repeating_map;
-
-std::mutex repeating_map_mutex;
-
-auto is_repeating(const std::string &number_str, auto rpt) -> bool {
-  {
-    std::lock_guard<std::mutex> lock(repeating_map_mutex);
-    if (repeating_map.contains(number_str) &&
-        repeating_map[number_str].contains(rpt)) {
-      return repeating_map[number_str][rpt];
-    }
-  }
+auto is_repeating(const std::string &number_str, long long rpt) -> bool {
   auto len = number_str.length();
-  if (rpt >= len) {
-    return false;
-  }
-  if (len % rpt != 0) {
+  if (rpt >= len || len % rpt != 0) {
     return false;
   }
   auto substr = number_str.substr(0, rpt);
-  for (auto i = rpt; i < len; i += rpt) {
+  for (size_t i = rpt; i < len; i += rpt) {
     if (number_str.substr(i, rpt) != substr) {
-      std::lock_guard<std::mutex> lock(repeating_map_mutex);
-      repeating_map[number_str][rpt] = false;
       return false;
     }
   }
-  std::lock_guard<std::mutex> lock(repeating_map_mutex);
-  repeating_map[number_str][rpt] = true;
   return true;
 }
 
 auto check_and_update(auto number) -> long long {
-  auto digits = number_of_digits(number);
-  auto divisors = get_divisors(digits);
   auto number_str = std::to_string(number);
+  auto digits = number_str.length();
+  const auto &divisors = get_divisors(digits);
   for (auto divisor : divisors) {
     if (is_repeating(number_str, divisor)) {
       return number;
@@ -112,6 +82,7 @@ auto check_and_update(auto number) -> long long {
 }
 
 auto main() -> int {
+  precompute_divisors();
   auto ranges = read_input();
   std::vector<std::jthread> threads;
   threads.reserve(ranges.size());
@@ -131,9 +102,9 @@ auto main() -> int {
   return 0;
 }
 
-// [koustav@i-0f9d48202555f1568 day-2]$ time ./build/puzzle-2
+// [koustav@i-0f9d48202555f1568 day-2 (master)]$ time ./build/puzzle-2
 // 24774350322
 
-// real    0m6.598s
-// user    0m6.053s
-// sys     1m26.356s
+// real    0m0.087s
+// user    0m1.304s
+// sys     0m0.006s
